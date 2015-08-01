c76d1e02038728527a9df3295ba31d72b51ff273
Rodrigo Vieira <rodrigo.conspena@gmail.com>
2015-08-01 11:13:15 -0300

    0.0.1
    - Commit inicial.

---
+++ AppWeb.ts
@@ -0,0 +1,18 @@
+﻿
+class AppWeb {
+    constructor(strNome: string) { }
+
+    private _strNome: string;
+
+    public get strNome(): string {
+        return this._strNome;
+    }
+
+    public set strNome(strNome: string) {
+        this._strNome = strNome;
+    }
+}
+
+var a = new AppWeb("teste");
+
+a.strNome = "Teste 2";
