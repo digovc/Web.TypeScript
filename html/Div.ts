3b1d1c0b7dac19648a22be9cb5245954aa58a10a
Rodrigo Vieira <rodrigo.conspena@gmail.com>
2015-10-15 22:27:14 -0300

    - Início da criação do menu principal.

---
+++ html/Div.ts
@@ -0,0 +1,33 @@
+﻿// #region Módulos externos
+
+/// <reference path="Tag.ts"/>
+
+// #endregion Módulos externos
+
+module NetZ.Web.TypeScript.html
+{
+    // #region Importações
+    // #endregion Importações
+
+    // #region Enumerados
+    // #endregion Enumerados
+
+    export class Div extends Tag
+    {
+        // #region Constantes
+        // #endregion Constantes
+
+        // #region Atributos
+        // #endregion Atributos
+
+        // #region Construtores
+
+        // #endregion Construtores
+
+        // #region Métodos
+        // #endregion Métodos
+
+        // #region Eventos
+        // #endregion Eventos
+    }
+}
---
+++ html/componente/ComponenteHtml.ts
@@ -0,0 +1,32 @@
+﻿// #region Módulos externos
+
+/// <reference path="../Div.ts"/>
+
+// #endregion Módulos externos
+
+module NetZ.Web.TypeScript.html.componente
+{
+    // #region Importações
+    // #endregion Importações
+
+    // #region Enumerados
+    // #endregion Enumerados
+
+    export class ComponenteHtml extends Div
+    {
+        // #region Constantes
+        // #endregion Constantes
+
+        // #region Atributos
+        // #endregion Atributos
+
+        // #region Construtores
+        // #endregion Construtores
+
+        // #region Métodos
+        // #endregion Métodos
+
+        // #region Eventos
+        // #endregion Eventos
+    }
+}
