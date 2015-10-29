e1767082f359fb9d25012944021d86967482e9fe
Rodrigo Vieira <rodrigo.conspena@gmail.com>
2015-10-28 22:47:46 -0200

    - Criação de vários arquivos para abstração de componente no browser.

---
+++ html/design/Tema.ts
@@ -0,0 +1,28 @@
+﻿/// <reference path="../../Objeto.ts"/>
+
+module NetZ.Web.TypeScript.html.design
+{
+    // #region Importações
+    // #endregion Importações
+
+    // #region Enumerados
+    // #endregion Enumerados
+
+    export class Tema extends Objeto
+    {
+        // #region Constantes
+        // #endregion Constantes
+
+        // #region Atributos
+        // #endregion Atributos
+
+        // #region Construtores
+        // #endregion Construtores
+
+        // #region Métodos
+        // #endregion Métodos
+
+        // #region Eventos
+        // #endregion Eventos
+    }
+}
