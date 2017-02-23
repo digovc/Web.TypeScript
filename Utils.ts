a185437d1d18fdd44dd83821744138d4af6de120
Rodrigo Daros <rodrigo.conspena@gmail.com>
2017-02-23 09:48:36 -0300

    Drive:
    - Implementações diversas.

--- Utils.ts
+++ Utils.ts
@@ -205,6 +205,38 @@
             return str;
         }
 
+        public static simplificar(str: string): string
+        {
+            if (Utils.getBooStrVazia(str))
+            {
+                return null;
+            }
+
+            str = str.toLowerCase();
+            str = str.trim();
+
+            var arrStrAcentos = ["ç", "á", "é", "í", "ó", "ú", "ý", "à", "è", "ì", "ò", "ù", "ã", "õ", "ñ", "ä", "ë", "ï", "ö", "ü", "ÿ", "â", "ê", "î", "ô", "û"];
+            var arrStrSemAcento = ["c", "a", "e", "i", "o", "u", "y", "a", "e", "i", "o", "u", "a", "o", "n", "a", "e", "i", "o", "u", "y", "a", "e", "i", "o", "u"];
+            var arrStrCaracteresEspeciais = ["\\.", "\\", ",", "-", ":", "\\(", "\\)", "ª", "\\|", "\\\\", "°", "^\\s+", "\\s+$", "\\s+", ".", "(", ")", "/"];
+
+            for (var i = 0; i < arrStrAcentos.length; i++)
+            {
+                str = Utils.replaceAll(str, arrStrAcentos[i], arrStrSemAcento[i]);
+            }
+
+            for (var i = 0; i < arrStrCaracteresEspeciais.length; i++)
+            {
+                str = Utils.replaceAll(str, arrStrCaracteresEspeciais[i], "");
+            }
+
+            if (Utils.getBooStrVazia(str))
+            {
+                return null;
+            }
+
+            return str.replace(" ", "_");
+        }
+
         /**
          * Verifica se o email contido em strEmail é válido. Retorna true
          * em caso afirmativo.
