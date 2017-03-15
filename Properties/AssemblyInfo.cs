93a92305a92d2b193ddc1993252cbccdf278c316
Rodrigo Daros <rodrigo.conspena@gmail.com>
2017-03-15 17:09:13 -0300

    Sentinela:
    - Adição de arquivos principais de design.
    - Adição das classes de controle de activities.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Web.TypeScript")]
+[assembly: AssemblyDescription("Biblioteca TypeScript para uso no cliente das aplicações construídas com o NetZ.Web.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Relatar Sistemas")]
+[assembly: AssemblyProduct("Web.TypeScript - Biblioteca TypeScript para uso no cliente das aplicações construídas com o NetZ.Web.")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6109e632-7aaf-4c20-83ac-a67bf83f2e61")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
